82639d830ead4865b20ddea395c100323cef133b
nikesh bhandari <nikesh.bhandari@midastechnologies.com.np>
2023-06-22 16:16:29 +0545

    sss

--- src/components/investigation/revenue/Revenue.js
+++ src/components/investigation/revenue/Revenue.js
@@ -1,43 +1,29 @@
-import React from 'react'
+import React from "react";
 import PatientTypeDetail from "./patientTypeDetail/PatientTypeDetail";
 import DepartmentWiseDetail from "./departmentWise/DepartmentWiseDetail";
-import TestWise from './testwise/TestWise';
-import CollectionDetails from './collectionDetails/CollectionDetails';
-import BillDetails from './billDetails/BillDetails';
-import RevenueDetailTwo from './RevenueDetailTwo';
-import RevenueDetail from './RevenueDetail';
+import TestWise from "./testwise/TestWise";
+import CollectionDetails from "./collectionDetails/CollectionDetails";
+import BillDetails from "./billDetails/BillDetails";
+import RevenueDetailTwo from "./RevenueDetailTwo";
+import RevenueDetail from "./RevenueDetail";
 
-
-
-function index() {
-
-
-
-
-
-
-
-
-
-  return ( 
-    <div className='mt-4'>
-    <div className='font-bold text-lg font-Poppins'>Revenue Details</div> 
-<RevenueDetail />
-    <div className='mt-5 grid grid-cols-2 gap-4'>
-      <PatientTypeDetail/>
-      <DepartmentWiseDetail/>
-      <TestWise/>
-      <div>
-        <CollectionDetails />
-        <BillDetails/>
+function Index() {
+  return (
+    <div className="mt-4">
+      <div className="font-Poppins text-lg font-bold">Revenue Details</div>
+      <RevenueDetail />
+      <div className="mt-5 grid grid-cols-2 gap-4">
+        <PatientTypeDetail />
+        <DepartmentWiseDetail />
+        <TestWise />
+        <div>
+          <CollectionDetails />
+          <BillDetails />
+        </div>
       </div>
+      <RevenueDetailTwo />
     </div>
-<RevenueDetailTwo />
-
-
-
-  </div>
-  )
+  );
 }
 
-export default index
+export default Index;
--- src/components/investigation/revenue/RevenueDetail.js
+++ src/components/investigation/revenue/RevenueDetail.js
@@ -1,72 +1,88 @@
-import React from 'react'
-import { Statistic } from 'antd';
+import React from "react";
+import { Statistic } from "antd";
 import chartup from "../../../../src/assets/chartup.png";
 import chartdown from "../../../../src/assets/chartdown.png";
-
+import CountUp from 'react-countup';
 const items = [
-    {
-      key: "1",
-      label: `Total Sales`,
-      value: `2,839`,
-      image: chartup,
-    },
-    {
-      key: "2",
-      label: `Total Refunds`,
-      value: `857`,
-      image: chartdown,
-    },
-    {
-      key: "3",
-      label: `Discounted Amount`,
-      value: `5,879`,
-      image: chartup,
-    },
-    {
-      key: "4",
-      label: `TPA's`,
-      value: `1,239`,
-      image: chartup,
-    },
-    {
-      key: "5",
-      label: `Deposits`,
-      value: `2,839`,
-      image: chartdown,
-    },
-
-  ];
+  {
+    key: "1",
+    label: `Total Sales`,
+    value: `2839`,
+    image: chartup,
+  },
+  {
+    key: "2",
+    label: `Total Refunds`,
+    value: `857`,
+    image: chartdown,
+  },
+  {
+    key: "3",
+    label: `Discounted Amount`,
+    value: `5879`,
+    image: chartup,
+  },
+  {
+    key: "4",
+    label: `TPA's`,
+    value: `1239`,
+    image: chartup,
+  },
+  {
+    key: "5",
+    label: `Deposits`,
+    value: `2839`,
+    image: chartdown,
+  },
+];
 
 function RevenueDetail() {
+  const formatter = (value) => <CountUp end={value} separator="," />;
   return (
-    <div className='grid grid-cols-5 mt-5 gap-x-3'>
-    {items.map(item => (
-   <div key={item.key} className='bg-white relative shadowdrop p-3 rounded-[10px] px-5'>
-   <div className='font-Poppins text-[#6B6B6B] xl:text-tiny text-sm'>{item.label}</div>
-   <div className='flex items-center justify-between'>
-   <div  className='font-bold font-Poppins text-[30px] xl:text-[40px]'>
-   <Statistic
-   
-   value={item.value}
-   precision={2}
-   valueStyle={{ color: '#000' }}
-   suffix={ <img src={item.image} className="absolute w-[40px] right-[10px] xl:right-[100px] top-[10%] xl:top-[50%]" style={{ width: "40px" }} alt="Loading.." /> }
-   
-   
- /> 
-   </div>
+    <div className="mt-5 grid grid-cols-5 gap-x-3">
+      {items.map((item) => (
+        <div
+          key={item.key}
+          className="bg-white shadowdrop relative rounded-[10px] p-3 px-5"
+        >
+          <div className="font-Poppins text-sm text-[#6B6B6B] xl:text-tiny">
+            {item.label}
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="font-Poppins text-[30px] font-bold xl:text-[40px]">
+              <Statistic
+                formatter={formatter}
+                value={item.value}
+                precision={2}
+                valueStyle={{ color: "#000" }}
+                suffix={
+                  <img
+                    src={item.image}
+                    className="absolute right-[10px] top-[10%] w-[40px] xl:right-[100px] xl:top-[50%]"
+                    style={{ width: "40px" }}
+                    alt="Loading.."
+                  />
+                }
+              />
+            </div>
 
-   <div>
-   <div className={`font-Poppins font-normal text-sm ${item.image == chartup ? 'text-[#2CB257]' : 'text-red'}`}>
-16.05%
-</div>       <div className='font-Poppins font-normal text-xs text-[#A8A8A8] mt-[2px]'>vs last 24 hours</div>
-   </div>
-
-   </div>
- </div>
-    ))}
-  </div>
-  )
+            <div>
+              <div
+                className={`font-Poppins text-sm font-normal ${
+                  item.image == chartup ? "text-[#2CB257]" : "text-red"
+                }`}
+              >
+                16.05%
+              </div>{" "}
+              <div className="mt-[2px] font-Poppins text-xs font-normal text-[#A8A8A8]">
+                vs last 24 hours
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default RevenueDetail
+export default RevenueDetail;
